1cfe04bc7533b57752c59d2f2e9076adf60bb3e6
TKubareva <tkubareva@korusconsulting.ru>
2020-04-06 01:36:59 +0300

    L1.Карточка обучающего курса

---
+++ lesson1/js/script.js
@@ -0,0 +1,57 @@
+new Vue({
+    el: "#app",
+    data: {
+        course: {
+            id: 1,
+            theme: "Тематика курса",
+            name: "Название курса" ,
+            description: "Описание курса",
+            date_start: new Date().getTime(),
+            href: "/string/",
+            student: [
+                    { id: 1, name: "Тест", lastname: "Тестов",  surname: "Тестович"},
+                    { id: 2, name: "Иван", lastname: "Иванов",  surname: "Иванович"}
+                ]
+            },
+
+        newStudent: {
+            name: '',
+            lastname: '',
+            surname: ''
+        },
+
+        show: false
+    },
+    computed: {
+        StudentAll () {
+            return this.course.student.length
+        },
+        localeDate() {
+            return (new Date(this.course.date_start)).toLocaleDateString()
+        },
+    },
+    methods: {
+        addNewStudent () {
+            if (this.newStudent.lastname.length >0) {
+                this.course.student.push({
+                    id: this.StudentAll.length + 1,
+                    name: this.newStudent.name,
+                    lastname: this.newStudent.lastname,
+                    surname: this.newStudent.surname,
+                });
+                this.show = false;
+                this.newStudent = {
+                    name: '',
+                    lastname: '',
+                    surname: ''
+                }
+            }
+        },
+
+        deleteStudent (userIndex) {
+            this.course.student.splice(userIndex, 1)
+        }
+    },
+    beforeDestroy () {},
+    destroyed () {}
+})
